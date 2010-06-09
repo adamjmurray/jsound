9f58a7c6edf9cd93c4594a9b0a0cf125552058da
Adam Murray <adamjmurray@gmail.com>
2010-06-08 21:03:40 -0700

    Monitor is now a proper Device and handles the <= method

--- lib/jsound/midi/devices/monitor.rb
+++ lib/jsound/midi/devices/monitor.rb
@@ -1,17 +1,20 @@
 # A device that prints out all incoming MIDI message.
 
 module JSound::Midi::Devices  
-  class Monitor
+  class Monitor < Device
     include_package 'javax.sound.midi'
     include javax.sound.midi.Receiver
     include JSound::Midi::Messages
     
-    def send(java_message, timestamp)
+    def send(java_message, timestamp=-1)
       message = Message.from_java(java_message) 
       puts message.to_s
     rescue
       STDERR.puts $!
     end
     
+    def <=(message)
+      self.send(message)
+    end
   end
 end
